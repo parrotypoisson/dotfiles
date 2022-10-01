1ee6e34a901810fdf823a6d07ab68f7a69f64d83
Italiana Mobster <71202170+parrotypoisson@users.noreply.github.com>
2022-10-01 10:47:18 -0400

    Initial nvim config

---
+++ lua/maps.lua
@@ -0,0 +1,2 @@
+vim.g.mapleader = ' '
+vim.keymap.set('i', 'jf', '<ESC>', {noremap = true})
---
+++ lua/plugins.lua
@@ -0,0 +1,13 @@
+return require('packer').startup(function(use)
+    use 'wbthomason/packer.nvim'
+    use 'tpope/vim-surround'
+    use {
+        'phaazon/hop.nvim',
+        branch = 'v2', -- optional but strongly recommended
+        config = function()
+          -- you can configure Hop the way you like here; see :h hop-config
+          require'hop'.setup { keys = 'etovxqpdygfblzhckisuran' }
+        end
+      }
+    use 'jamestthompson3/nvim-remote-containers'
+end)
